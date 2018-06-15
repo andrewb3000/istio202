4510ab256d012b32f7b1f13dfba318af4cea9b2f
Andrii Bryndzak <abryndzak@gmail.com>
2018-06-15 14:41:05 +0300

    finalizing kube deployment

--- weather/gweather-ui/ui-webapp.py
+++ weather/gweather-ui/ui-webapp.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import abort
 import requests
 import os
+import time
 
 WEATHER_API_URL=os.environ['WEATHER_API_URL']
 # WEATHER_API_URL='http://192.168.56.58:8000/weather'
@@ -17,7 +18,10 @@ def weather():
     try:
         weather_info = requests.get(WEATHER_API_URL)
         if weather_info.status_code == 200:
-            return weather_info.content
+            print(type(weather_info.content))
+            output = time.ctime() + ' <br> \n' + weather_info.content.decode('utf-8')
+            # return weather_info.content
+            return output
         else:
             abort(weather_info.status_code)
     except Exception as e:
